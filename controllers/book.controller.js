af3ee1cdb5c920b15ba4c190d94a1bb40fc1807c
Kshitij Kotasthane <kshitij.kotasthane@gmail.com>
2022-03-18 21:01:46 +0530

    books can be rented, returned and deleted

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,4 +1,7 @@
 const Book = require("../models/").book;
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = "my_secret";
 
 const getBooks = (req, res, next) => {
   Book.findAll()
@@ -45,6 +48,109 @@ const createBook = (req, res, next) => {
     });
 };
 
+const mutateBook = (req, res, next) => {
+  const bookId = req.body.bookId;
+  const decodedToken = jwt.verify(req.headers.authorization, SECRET_KEY);
+
+  if (req.query.delete === "true") {
+    Book.findOne({ where: { id: bookId } })
+      .then((bookFound) => {
+        //if book exists delete it
+        Book.destroy({ where: { id: bookFound.id } })
+          .then(() => {
+            res
+              .status(200)
+              .json({ message: `book with id ${bookId} has been deleted` });
+          })
+          .catch((err) => {
+            res.status(500).json({ message: err.message });
+          });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  } else if (req.query.rent === "true") {
+    Book.findAll({
+      where: {
+        rentedBy: decodedToken.userId,
+      },
+    })
+      .then((booksFound) => {
+        if (booksFound.length !== 2) {
+          //check if book is available to rent
+          Book.findOne({ where: { id: bookId } })
+            .then((bookFound) => {
+              if (!bookFound.rentedBy) {
+                Book.update(
+                  { rentedBy: decodedToken.userId },
+                  { where: { id: bookId } }
+                )
+                  .then(() => {
+                    res.status(200).json({
+                      message: `book with id ${bookId} has been rented to you`,
+                    });
+                  })
+                  .catch((err) => {
+                    res.status(500).json({ message: err.message });
+                  });
+              } else {
+                res.status(400).json({ message: `that book cannot be rented` });
+              }
+            })
+            .catch((err) => {
+              res.status(500).json({ message: err.message });
+            });
+        } else {
+          res.status(400).json({
+            message: `you cannot rent anymore books, return some to rent more`,
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  } else if (req.query.return === "true") {
+    Book.update(
+      { rentedBy: null },
+      {
+        where: { id: bookId },
+      }
+    )
+      .then(() => {
+        res.status(200).json({
+          message: `book with id ${bookId} has been returned`,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  } else {
+    res.status(400).json({
+      message: `that's not a valid option`,
+    });
+  }
+};
+
+const canRent = (req, res, next) => {
+  let bookId = req.params.bookId;
+
+  Book.findOne({
+    where: {
+      id: bookId,
+    },
+  })
+    .then((bookFound) => {
+      let canRent = false;
+      if (!bookFound.rentedBy) {
+        canRent = true;
+      }
+      res.status(200).json({ canRent: canRent });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
 const getRentedBooks = (req, res, next) => {
   const userId = req.params.userId;
   Book.findAll({
@@ -60,8 +166,30 @@ const getRentedBooks = (req, res, next) => {
     });
 };
 
+const deleteBook = (req, res, next) => {
+  const bookId = req.params.bookId;
+  Book.findOne({ where: { id: bookId } })
+    .then((bookFound) => {
+      //if book exists delete it
+      Book.destroy({ where: { id: bookFound.id } })
+        .then(() => {
+          res
+            .status(200)
+            .json({ message: `book with id ${bookId} has been deleted` });
+        })
+        .catch((err) => {
+          res.status(500).json({ message: err.message });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+};
+
 module.exports = {
   getBooks,
   createBook,
   getRentedBooks,
+  canRent,
+  mutateBook,
 };
